9ef2484d44850d46baa0e53e437ecca135225eb9
giorgipro55 <tglunchadze84@gmail.com>
2024-11-20 11:39:56 -0800

    commit

---
+++ day 7/classvork/classwork 7.py
@@ -0,0 +1,14 @@
+name=input("შეიტანრთ თქვენი სახელი")
+name=input("შეიტანრთ თქვენი პაროლი")
+age=input("შეიტანრთ თქვენი წლოვანება")
+
+favourite_namber=input("საყვარელი ციფრი")
+your_frend_favourite_namber=input("შენი მეგობრის საყვარელი ციფრი")
+გამოტვლა=input (favourite_namber+your_frend_favourite_namber)
+გამოტვლ=input (favourite_namber-your_frend_favourite_namber)
+გამოტვ=input (favourite_namber/your_frend_favourite_namber)
+
+
+
+gmail=input("თქვენი გამილი   jumberatyabladze5685685@gmail.com")
+pasword=input("თქვენი პაროლი   12345")
---
+++ day 7/homework/homework 7.py
@@ -0,0 +1,10 @@
+name=input("შეიტანრთ თქვენი სახელი")
+lastname=input("შეიტანრთ თქვენი გვარი")
+age=input("შეიტანრთ თქვენი წლოვანება")
+
+favourite_namber=input("საყვარელი ციფრი")
+your_frend_favourite_namber=input("შენი მეგობრის საყვარელი ციფრი")
+გამოტვლა=input (favourite_namber+your_frend_favourite_namber)
+
+gmail=input("თქვენი გამილი   jumberatyabladze5685685@gmail.com")
+pasword=input("თქვენი პაროლი   12345")
