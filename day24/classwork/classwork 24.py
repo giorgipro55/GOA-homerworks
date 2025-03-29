f1a021c25159c1b4ca46fd6975ea6bebeeda7c39
giorgipro55 <tglunchadze84@gmail.com>
2025-03-29 03:40:55 -0700

    commit

---
+++ day24/classwork/classwork 24.py
@@ -0,0 +1,9 @@
+for i in range(21):
+    continue 
+    print(i)
+num = 0
+while True:
+    num += 1
+    print(num)
+    if num == 14:
+        break
---
+++ day24/homework/homework 24.py
@@ -0,0 +1,9 @@
+for i in range(31):
+    continue 
+    print(i)
+num = 0
+while True:
+    num += 3
+    print(num)
+    if num == 15:
+        break
